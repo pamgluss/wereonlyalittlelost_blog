3218f237ef0f7650ff39c2acbad1315cf650081b
Pam <pamela@gluss.com>
2021-01-18 13:25:35 -0800

    mills canyon nature area fix commit history and social media updater

---
+++ content/trips/california/mills-canyon-nature-area.md
@@ -0,0 +1,20 @@
+---
+title: "Mills Canyon Nature Area"
+date: 2021-01-17
+tags: ['California','United States', 'Hiking', '2021 State Parks']
+difficulty: 'Easy'
+rating: 'Not Recommended'
+draft: false
+---
+
+First post of 2021! Welcome to the new year y'all! This year my focus is definitely going to be on things inside my house or within driving distance so it is gonna be a quiet year for the blog. I'm thinking this will age better than [this post from 2019...](https://wereonlyalittlelost.tumblr.com/post/189990481466/happy-2020)
+
+When it was coming down to the wire on my 2020 hiking goals, I heard about some of the Peninsula city parks and put them in my tracking document. I live in Burlingame, so [Mills Canyon](https://bahiker.com/southbayhikes/mills.html) being only a few miles from me was appealing. I think you get what you ask for when you hike a city park. It is very small and throughout the 1.4 mile loop you can see people's houses and encounter roads.
+
+That's not to say this isn't pretty. The trail encounters a stream, there are trees and rocks. There is a decent overlook.
+
+![20210117_102757](/images/20210117_102757.jpg)
+
+I just think when you can drive 20 minutes and be in the Santa Cruz mountains that you would only come here if you lived very close by.
+
+Since the hike was so short, we ended up driving over to the [Bay Trail](https://goo.gl/maps/GcYHzMLdCyequK7m6) to walk along the bay. If you're looking for another thing to fill your day in Burlingame, that's a really lovely walk along the water with great views. Downtown Burlingame is very close by to there as well.
---
+++ social-media.py
@@ -0,0 +1,94 @@
+import pytumblr
+import sys
+from datetime import datetime
+import os
+from git import Repo
+import requests
+import json
+
+# for local testing
+from dotenv import load_dotenv
+from pathlib import Path  # Python 3.6+ only
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+BASE_URL = 'http://wereonlyalittlelost.com/'
+
+# Get the recent changed images from the git commit
+repo = Repo(os.getenv('TRAVIS_BUILD_DIR'))
+
+diff = repo.git.diff('HEAD~1..HEAD', name_only=True)
+split_lines = diff.splitlines()
+
+# Link to the specific post instead of to the homepage
+# So search for .md files and remove any from about/ then reformat the path to match what you'll see in prod
+# To Future Pam, if you're editing multiple .md files under trips at once...... good luck
+md_filtered_list = list(filter(lambda x: '.md' in x, split_lines))
+
+for md in md_filtered_list:
+    if ('about' in md):
+        md_filtered_list.remove(md)
+
+# The run.sh script checks to make sure there's a change to .md but if we *only* change about, we should skip posting
+if len(md_filtered_list) == 0:
+    print('Did not find any md files besides About. Exiting early.')
+    quit()
+
+url = md_filtered_list[0].replace('content/', '')[0:-3] + "/"
+
+# Let's see if we can get the tags and some content out of the md too
+# We're currently doing idiotic things to remove unnecessary characters
+f = open(md_filtered_list[0], 'r')
+fileString = f.read()
+start = fileString.find('tags:')
+end = fileString.find('\n', start)
+tags = "".join(fileString[start:end].replace('tags: ', ''))
+
+titleStart = fileString.find('title:')
+titleEnd = fileString.find('\n', titleStart) - 1
+title = "".join(fileString[titleStart:titleEnd].replace('title: "', ''))
+
+# Connect to Tumblr Oath client
+print('Connecting to Tumblr Oauth client')
+client = pytumblr.TumblrRestClient(
+    os.getenv('TUMBLR_CONSUMER_KEY'),
+    os.getenv('TUMBLR_CONSUMER_SECRET'),
+    os.getenv('TUMBLR_TOKEN'),
+    os.getenv('TUMBLR_TOKEN_SECRET'),
+)
+
+jpg_filtered_list = list(filter(lambda x: '.jpg' in x, split_lines))
+
+if len(jpg_filtered_list) > 0:
+    selected_image = jpg_filtered_list[0] 
+    client.create_photo(
+        'wereonlyalittlelost', 
+        state="published",
+        tags=tags,
+        format="markdown",
+        data=[selected_image],
+        caption='## {title} \n' + "Read more here: [{BASE_URL}{url}]({BASE_URL}{url})"
+    )
+    print('Uploaded this image to Tumblr: ' + selected_image)
+else:
+    client.create_link(
+        'wereonlyalittlelost', 
+        state="published", 
+        title=title, 
+        url='http://wereonlyalittlelost.com/' + url,
+        description="Read more here: [{BASE_URL}{url}]({BASE_URL}{url})"
+    )
+    print('Uploaded a link to Tumblr')
+
+# Use python Request lib to upload data to Discord/Slack instead of a terminal cURL that depends on the commit message
+slackData = json.dumps({ "text": f"{title}: Read more here http://wereonlyalittlelost.com/{url}" })
+rS = requests.post(os.getenv('SLACK_WEBHOOK_URL'), data = slackData, headers={'Content-Type': 'application/json'})
+if rS.text == "ok":
+    print('Successfully uploaded to Slack!')
+else:
+    print('Something went wrong uploading to Slack')
+
+# Discord response can be 200-204
+data = {'content': f"{title}: Read more here http://wereonlyalittlelost.com/{url}"}
+rD = requests.post(os.getenv('DISCORD_WEBHOOK_URL'), data = data)
+print('Uploaded to Discord!')
